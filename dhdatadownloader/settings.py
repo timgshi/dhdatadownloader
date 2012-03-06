b4bf73bedd3c1de25d2794ce9d13583454b6910c
Tim Shi <timgshi@gmail.com>
2012-03-06 13:47:57 -0800

    installed bootstrap form

--- dhdatadownloader/settings.py
+++ dhdatadownloader/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrapped',
+    'bootstrap',
     'xlwt',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
